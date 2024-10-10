df0f51259638bbd58f80ee8b358dcdc12a904cd0
jorgef29 <jorgefigueredo2001@gmail.com>
2024-10-10 11:48:30 -0300

    first commit

---
+++ src/main/java/com/fiuni/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,8 @@
+package com.fiuni.api_gateway.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+}
---
+++ src/main/java/com/fiuni/api_gateway/filter/RequestFilter.java
@@ -0,0 +1,4 @@
+package com.fiuni.api_gateway.filter;
+
+public class RequestFilter {
+}
